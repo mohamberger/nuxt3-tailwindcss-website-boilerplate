63221c487cb3300bb42f28b41179453e35588011
Moritz Hamberger <mo@allesdigital.io>
2024-10-04 22:55:08 +0200

    add plausible module to track events and page views

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,5 +21,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["@/assets/css/fonts.css"],
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/seo", "@nuxt/image"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/seo", "@nuxt/image", "@nuxtjs/plausible"],
 });
